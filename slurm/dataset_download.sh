5d209ebc051ccbf1c11e5361e175fa5d9d7d7e5b
Patrick Kage <patrick.r.kage@gmail.com>
2025-02-13 17:32:01 -0500

    add resnet101 model

--- slurm/dataset_download.sh
+++ slurm/dataset_download.sh
@@ -7,14 +7,14 @@ echo "downloading cityscapes"
 
 if ! [ -f $LSCRATCH/datasets/gtFine_trainvaltest.zip ]; then
     echo "gtFine_trainvaltest.zip not found, downloading..."
-    curl -L $DOWNLOAD_FUNCTION_URL/gtFine_trainvaltest.zip -o $LSCRATCH/datasets/gtFine_trainvaltest.zip
+    curl -L $DOWNLOAD_FUNCTION_URL/datasets/gtFine_trainvaltest.zip -o $LSCRATCH/datasets/gtFine_trainvaltest.zip
 else
     echo "gtFine_trainvaltest.zip has already been downloaded."
 fi
 
 if ! [ -f $LSCRATCH/datasets/gtCoarse.zip ]; then
     echo "gtCoarse.zip not found, downloading..."
-    curl -L $DOWNLOAD_FUNCTION_URL/gtCoarse.zip -o $LSCRATCH/datasets/gtCoarse.zip
+    curl -L $DOWNLOAD_FUNCTION_URL/datasets/gtCoarse.zip -o $LSCRATCH/datasets/gtCoarse.zip
 else
     echo "gtCoarse.zip has already been downloaded."
 fi
@@ -43,8 +43,8 @@ fi
 echo "downloading coco"
 if ! [ -f $LSCRATCH/datasets/coco/train2014.zip ]; then
     echo "COCO train2014.zip not found, downloading..."
-    curl -LJ http://images.cocodataset.org/zips/train2014.zip -o $LSCRATCH/datasets/coco/train2014.zip
-    curl -L $DOWNLOAD_FUNCTION_URL/coco_train2014.zip -o $LSCRATCH/datasets/coco/train2014.zip
+    # curl -LJ http://images.cocodataset.org/zips/train2014.zip -o $LSCRATCH/datasets/coco/train2014.zip
+    curl -L $DOWNLOAD_FUNCTION_URL/datasets/coco_train2014.zip -o $LSCRATCH/datasets/coco/train2014.zip
 else
     echo "COCO train2014.zip has already been downloaded."
 fi
@@ -56,6 +56,19 @@ else
 fi
 
 
+echo "downloading weights..."
+if ! [ -f $LSCRATCH/pretrained/best_deeplabv3plus_resnet101_cityscapes_os16.pth ]; then
+    echo "best_deeplabv3plus_resnet101_cityscapes_os16.pth not found, downloading..."
+    curl -L $DOWNLOAD_FUNCTION_URL/checkpoints/upstream/best_deeplabv3plus_resnet101_cityscapes_os16.pth -o $LSCRATCH/pretrained/best_deeplabv3plus_resnet101_cityscapes_os16.pth
+else
+    echo "best_deeplabv3plus_resnet101_cityscapes_os16.pth has already been downloaded."
+fi
+if ! [ -d $LSCRATCH/datasets/coco/train2014 ]; then
+    echo "COCO train2014.zip has not been unzipped, expanding now..."
+    cd $LSCRATCH/datasets/coco && unzip train2014.zip
+else
+    echo "train2014.zip has already been unzipped."
+fi
 
 # ... and the voc
 # if ! [ -f ./VOCtrainval_11-May-2012.tar ]; then
