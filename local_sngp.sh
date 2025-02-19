2d89ee0cb436a1d6f07ed9b91c87588da6f33bb9
jroth <jay.c.rothenberger@gmail.com>
2025-02-19 12:20:53 -0600

    format update scripts

--- local_sngp.sh
+++ local_sngp.sh
@@ -1,5 +1,18 @@
-export WANDB_PROJECT=ai2es
+export WANDB_ENTITY=ai2es
+export WANDB_PROJECT=unc-pl-seg
 export LSCRATCH=./lscratch
 export WANDB_API_KEY=6ac799cb76304b17ce74f5161bc27f7a80b6ecee
 
-torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --standalone --nnodes=1 --nproc-per-node=1 main_ddp.py --batch_size 16 --test_batch_size 8 --epochs 64 --model sngp --ul_fraction 0.5 --strategy self --accumulate 1
+torchrun --rdzv-backend=c10d \
+        --rdzv-endpoint=localhost:0 \
+        --standalone \
+        --nnodes=1 \
+        --nproc-per-node=1 \
+        main_ddp.py \
+        --batch_size 4 \
+        --test_batch_size 4 \
+        --epochs 32 \
+        --model deep_ensemble \
+        --ul_fraction 0.4 \
+        --strategy self \
+        --accumulate 4 \
