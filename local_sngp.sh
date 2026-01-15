9c4f1d411d0c8ccf4895fedb54d274ada1998895
jroth <jay.c.rothenberger@gmail.com>
2026-01-15 15:48:16 -0600

    add a new script for testing the sngp code

--- local_sngp.sh
+++ local_sngp.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export WANDB_ENTITY=ai2es
 export WANDB_PROJECT=unc-pl-seg
 export LSCRATCH=./lscratch
@@ -16,3 +17,10 @@ torchrun --rdzv-backend=c10d \
         --ul_fraction 0.4 \
         --strategy self \
         --accumulate 4 \
+=======
+export WANDB_PROJECT=ai2es
+export LSCRATCH=./lscratch
+export WANDB_API_KEY=6ac799cb76304b17ce74f5161bc27f7a80b6ecee
+
+torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --standalone --nnodes=1 --nproc-per-node=1 main_ddp.py --batch_size 16 --test_batch_size 8 --epochs 64 --model sngp --ul_fraction 0.5 --strategy self --accumulate 1
+>>>>>>> 18016db (add a new script for testing the sngp code)
